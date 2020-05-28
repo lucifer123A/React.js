bc3280386161c2215694f484f651fb92af423375
Lokesh Dulani <lokeshdulani91@gmail.com>
2020-05-28 00:31:22 -0700

    SPA Part-2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,65 +1,72 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
-import {Media} from "reactstrap";
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-
-    function RenderDish({ddish}){
+    function RenderDish({dish}){
         return(
-            <Card className="ml-5">
-                    <CardImg top src={ddish.image} alt={ddish.name}/>
+            <Card className="col-12 col-md-5 m-1">
+                    <CardImg top src={dish.image} alt={dish.name}/>
                         <CardBody>
-                            <CardTitle>{ddish.name}</CardTitle>
-                            <CardText>{ddish.description}</CardText>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
                         </CardBody>
             </Card>
         );
     }
 
-    function RenderComments({ddish}){
+    function RenderComments({comments}){
+        if(comments!=null){
                 return(
-                <div >
-                    <Media tag='li'>
-                        <Media body>
-                            <p>{ddish.comment}</p>
-                            <p>--{ddish.author}, {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(ddish.date)))}</p>
-                        </Media>
-                    </Media>
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment)=>{
+                            return(
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>--{comment.author}, {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
+                        
+                    </ul>
                 </div>
                 )
+        }
+        else{
+            return(
+                <div></div>
+            )
+        }
     }
 
 
     const DishDetail=(props)=>{
         if(props.dish!=null)
         {
-        const ccomments = props.dish.comments.map((c)=>{
             return(
-                    <div key={c.id}>
-                        <RenderComments ddish={c}/>
-                    </div>   
-            )
-        });
-            return(
-                <div className="row">
-
-                    <div className="col-12 col-md-5 m-1">
-                            <RenderDish ddish={props.dish}/>
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>     
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr/>
+                        </div>
                     </div>
-
-                    <div className="col-12 col-md-5 m-1">
-                        <Media list>
-                            <h4>Comments</h4>
-                            {ccomments}
-                        </Media>
+                    <div className="row">
+                        <RenderDish dish={props.dish}/>                
+                        <RenderComments comments={props.comments}/>
                     </div>
-                
                 </div> 
-            )
+            );
         }
         else{
             return(
                 <div></div>
-            )
+            );
         }
         }
     
