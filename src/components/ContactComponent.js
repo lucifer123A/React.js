5b7b6d5d9405fb5b8ff06035cad436ccf6f639fb
Lokesh Dulani <lokeshdulani91@gmail.com>
2020-06-01 05:31:25 -0700

    just testing

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -17,6 +17,8 @@ class Contact extends React.Component{
     }
     
 
+
+    
     handleSubmit(values){
         console.log("Current state is: "+JSON.stringify(values));
         alert("Current state is: "+JSON.stringify(values));
