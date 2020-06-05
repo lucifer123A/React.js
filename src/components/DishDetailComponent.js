21fa048a9cfd63c939b8ae2223c204989c448588
Lokesh Dulani <lokeshdulani91@gmail.com>
2020-06-05 05:45:50 -0700

    Fetch Post Comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends React.Component{
 
     handleSubmit(val){
         this.toggleModal();
-        this.props.addComment(this.props.dishId, val.rating, val.author, val.comment)
+        this.props.postComment(this.props.dishId, val.rating, val.author, val.comment)
     }
 
     render(){
@@ -106,7 +106,7 @@ class CommentForm extends React.Component{
         );
     }
 
-    function RenderComments({comments, addComment, dishId}){
+    function RenderComments({comments, postComment, dishId}){
         if(comments!=null){
                 return(
                 <div>
@@ -122,7 +122,7 @@ class CommentForm extends React.Component{
                         })}
                         
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
                 )
         }
@@ -175,7 +175,7 @@ class CommentForm extends React.Component{
 
                         <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}
                             />
                         </div>        
